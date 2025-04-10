b747e214a062040a7abbf414d9c46e6d0bc94624
rusefillc <rusefillc@gmail.com>
2025-04-10 18:09:49 -0400

    case?

--- generated/controllers/generated/signature_XYZ.h
+++
@@ -1,6 +0,0 @@
-//
-// was generated automatically by rusEFI tool config_definition-all.jar based on gen_config.sh by SignatureConsumer
-//
-
-#define SIGNATURE_HASH 1926089875
-#define TS_SIGNATURE "rusEFI main.2025.04.10.XYZ.1926089875"
--- generated/controllers/generated/signature_uaefi-OBD1.h
+++
@@ -1,6 +0,0 @@
-//
-// was generated automatically by rusEFI tool config_definition-all.jar based on gen_config.sh by SignatureConsumer
-//
-
-#define SIGNATURE_HASH 2985574963
-#define TS_SIGNATURE "rusEFI main.2025.04.10.uaefi-OBD1.2985574963"
