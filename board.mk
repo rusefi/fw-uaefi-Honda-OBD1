ec9571e2a811d7517955b279b374d9813a3c6fc7
rusefillc <rusefillc@gmail.com>
2025-04-10 16:25:43 -0400

    progress

--- board.mk
+++ board.mk
@@ -6,6 +6,8 @@ BOARDINC += $(BOARD_DIR)/generated/controllers/generated \
     $(BOARD_DIR)/ext/rusefi/firmware/config/boards/hellen/uaefi121/ \
 
 
+DDEFS += -DDEFAULT_ENGINE_TYPE=engine_type_e::HONDA_OBD1
+
 # defines SHORT_BOARD_NAME
 include $(BOARD_DIR)/meta-info.env
 
