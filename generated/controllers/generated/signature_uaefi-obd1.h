1588205a03c7293d2d80f103a4bd43aa0c4ac548
GitHub git update Action <action@github.com>
2025-04-11 21:51:56 +0000

    Custom Auto-generated Configs
    [no ci]

--- generated/controllers/generated/signature_uaefi-obd1.h
+++ generated/controllers/generated/signature_uaefi-obd1.h
@@ -2,5 +2,5 @@
 // was generated automatically by rusEFI tool config_definition-all.jar based on gen_config.sh by SignatureConsumer
 //
 
-#define SIGNATURE_HASH 2066971671
-#define TS_SIGNATURE "rusEFI main.2025.04.11.uaefi-obd1.2066971671"
+#define SIGNATURE_HASH 4061078152
+#define TS_SIGNATURE "rusEFI main.2025.04.11.uaefi-obd1.4061078152"
