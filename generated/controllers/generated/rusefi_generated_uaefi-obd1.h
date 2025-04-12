451ed4e10c4cceb423829f849b33efaebf79fefa
GitHub git update Action <action@github.com>
2025-04-12 20:38:24 +0000

    Custom Auto-generated Configs
    [no ci]

--- generated/controllers/generated/rusefi_generated_uaefi-obd1.h
+++ generated/controllers/generated/rusefi_generated_uaefi-obd1.h
@@ -1384,7 +1384,7 @@
 #define SentInput_NONE 0
 #define show_tcu_gauges false
 #define show_vvt_output_pin true
-#define SIGNATURE_HASH 3752153728
+#define SIGNATURE_HASH 3813597717
 #define SIMULATOR_TUNE_BIN_FILE_NAME "generated/simulator_tune_image.bin"
 #define SIMULATOR_TUNE_BIN_FILE_NAME_PREFIX "generated/simulator_tune_image"
 #define SIMULATOR_TUNE_BIN_FILE_NAME_SUFFIX ".bin"
@@ -1723,7 +1723,7 @@
 #define ts_show_can_wbo true
 #define ts_show_charge_estimation true
 #define ts_show_check_engine false
-#define ts_show_clt_iat_pullup true
+#define ts_show_clt_iat_pullup false
 #define ts_show_clutch_down true
 #define ts_show_clutch_down_pin true
 #define ts_show_clutch_up true
@@ -1830,7 +1830,7 @@
 #define ts_show_sd_card true
 #define ts_show_sd_pins true
 #define ts_show_second_bank true
-#define ts_show_sent true
+#define ts_show_sent false
 #define ts_show_software_knock false
 #define ts_show_speedo_settings true
 #define ts_show_spi true
@@ -1869,22 +1869,22 @@
 #define ts_show_tunerstudio_port true
 #define ts_show_useRunningMathForCranking true
 #define ts_show_useSeparateVeForIdle true
-#define ts_show_vbatt true
+#define ts_show_vbatt false
 #define ts_show_ve_blend true
 #define ts_show_veBlends2 true
 #define ts_show_veBlends3 true
 #define ts_show_veBlends4 true
 #define ts_show_vehicle_speed_sensor true
 #define ts_show_vr_threshold_2 false
-#define ts_show_vr_threshold_all false
-#define ts_show_vr_threshold_pins true
+#define ts_show_vr_threshold_all true
+#define ts_show_vr_threshold_pins false
 #define ts_show_vvt_frequency false
 #define ts_show_vvt_output true
 #define ts_show_vvt_output_pin false
 #define ts_show_wastegate_sensor true
 #define ts_show_wbo_canbus_index true
 #define ts_show_wbo_canbus_set_index true
-#define TS_SIGNATURE "rusEFI main.2025.04.12.uaefi-obd1.3752153728"
+#define TS_SIGNATURE "rusEFI main.2025.04.12.uaefi-obd1.3813597717"
 #define TS_SIMULATE_CAN '>'
 #define TS_SIMULATE_CAN_char >
 #define TS_SINGLE_WRITE_COMMAND 'W'
