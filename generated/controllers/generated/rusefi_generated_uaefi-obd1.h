29ef541f548c2ff4a52d7dbb8febf8e857693ea7
GitHub git update Action <action@github.com>
2025-06-07 00:20:59 +0000

    Custom Auto-generated Configs
    [no ci]

--- generated/controllers/generated/rusefi_generated_uaefi-obd1.h
+++ generated/controllers/generated/rusefi_generated_uaefi-obd1.h
@@ -1449,7 +1449,7 @@
 #define SentInput_NONE 0
 #define show_tcu_gauges false
 #define show_vvt_output_pin true
-#define SIGNATURE_HASH 2466679281
+#define SIGNATURE_HASH 4239565675
 #define SIMULATOR_TUNE_BIN_FILE_NAME "generated/simulator_tune_image.bin"
 #define SIMULATOR_TUNE_BIN_FILE_NAME_PREFIX "generated/simulator_tune_image"
 #define SIMULATOR_TUNE_BIN_FILE_NAME_SUFFIX ".bin"
@@ -1986,7 +1986,7 @@
 #define ts_show_wastegate_sensor true
 #define ts_show_wbo_canbus_index true
 #define ts_show_wbo_canbus_set_index true
-#define TS_SIGNATURE "rusEFI main.2025.06.06.uaefi-obd1.2466679281"
+#define TS_SIGNATURE "rusEFI main.2025.06.07.uaefi-obd1.4239565675"
 #define TS_SIMULATE_CAN '>'
 #define TS_SIMULATE_CAN_char >
 #define TS_SINGLE_WRITE_COMMAND 'W'
--- generated/controllers/generated/signature_uaefi-obd1.h
+++ generated/controllers/generated/signature_uaefi-obd1.h
@@ -2,5 +2,5 @@
 // was generated automatically by rusEFI tool config_definition-all.jar based on gen_config.sh by SignatureConsumer
 //
 
-#define SIGNATURE_HASH 2466679281
-#define TS_SIGNATURE "rusEFI main.2025.06.06.uaefi-obd1.2466679281"
+#define SIGNATURE_HASH 4239565675
+#define TS_SIGNATURE "rusEFI main.2025.06.07.uaefi-obd1.4239565675"
