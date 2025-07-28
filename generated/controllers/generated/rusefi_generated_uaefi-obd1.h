ed56d86e916389be3adcd4390a665445475cc147
GitHub git update Action <action@github.com>
2025-07-28 00:24:52 +0000

    Custom Auto-generated Configs
    [no ci]

--- generated/controllers/generated/rusefi_generated_uaefi-obd1.h
+++ generated/controllers/generated/rusefi_generated_uaefi-obd1.h
@@ -2042,7 +2042,7 @@
 #define ts_show_wastegate_sensor true
 #define ts_show_wbo_canbus_index true
 #define ts_show_wbo_canbus_set_index true
-#define TS_SIGNATURE "rusEFI main.2025.07.27.uaefi-obd1.3079963165"
+#define TS_SIGNATURE "rusEFI main.2025.07.28.uaefi-obd1.3079963165"
 #define TS_SIMULATE_CAN '>'
 #define TS_SIMULATE_CAN_char >
 #define TS_TEST_COMMAND 't'
