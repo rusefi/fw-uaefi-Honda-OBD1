833fd1a5926f886560c909d0fe1f5563c2a64c4b
GitHub git update Action <action@github.com>
2025-11-01 00:25:28 +0000

    Custom Auto-generated Configs
    [no ci]

--- generated/controllers/generated/rusefi_generated_uaefi-obd1.h
+++ generated/controllers/generated/rusefi_generated_uaefi-obd1.h
@@ -1542,7 +1542,7 @@
 #define SentInput_NONE 0
 #define show_tcu_gauges false
 #define show_vvt_output_pin true
-#define SIGNATURE_HASH 770758477
+#define SIGNATURE_HASH 1218608077
 #define SIMULATOR_TUNE_BIN_FILE_NAME "generated/simulator_tune_image.bin"
 #define SIMULATOR_TUNE_BIN_FILE_NAME_PREFIX "generated/simulator_tune_image"
 #define SIMULATOR_TUNE_BIN_FILE_NAME_SUFFIX ".bin"
@@ -2123,7 +2123,7 @@
 #define ts_show_wastegate_sensor true
 #define ts_show_wbo_canbus_index true
 #define ts_show_wbo_canbus_set_index true
-#define TS_SIGNATURE "rusEFI main.2025.10.31.uaefi-obd1.770758477"
+#define TS_SIGNATURE "rusEFI main.2025.11.01.uaefi-obd1.1218608077"
 #define TS_SIMULATE_CAN '>'
 #define TS_SIMULATE_CAN_char >
 #define TS_TEST_COMMAND 't'
--- generated/controllers/generated/signature_uaefi-obd1.h
+++ generated/controllers/generated/signature_uaefi-obd1.h
@@ -2,5 +2,5 @@
 // CHeaderConsumer was generated automatically by rusEFI tool config_definition-all.jar based on gen_config.sh by SignatureConsumer
 //
 
-#define SIGNATURE_HASH 770758477
-#define TS_SIGNATURE "rusEFI main.2025.10.31.uaefi-obd1.770758477"
+#define SIGNATURE_HASH 1218608077
+#define TS_SIGNATURE "rusEFI main.2025.11.01.uaefi-obd1.1218608077"
